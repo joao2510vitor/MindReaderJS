e4d3ab8c5c469098dc7d96d03baad2293f924281
João Vitor da Silva <joao2510vitor@outlook.com>
2024-10-30 11:13:42 -0300

    refactor(ui): substituir alert e prompt por interação via HTML com input e funções
    Refatorei a lógica do projeto para melhorar a experiência do usuário e organizar o código. A interação, que antes era feita através de `alert` e `prompt`, agora acontece diretamente na interface HTML usando elementos `input`, manipulação com `document.querySelector`, e atualizações dinâmicas com `innerHTML`.
    
    Além disso, o código foi modularizado com funções, o que facilita a manutenção e a expansão futura. Essas mudanças substituem o código original, mantendo as mesmas funcionalidades de forma mais eficiente e organizada.

--- game/script.js
+++ game/script.js
@@ -1,30 +1,39 @@
-alert('Boas vindas ao jogo do número secreto!');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random()* numeroMaximo + 1);
-let chute;
+let numeroSecreto = gerarNumeroAleatorio(10);
 let tentativas = 1;
-console.log(numeroSecreto);
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Mind Reader';
+//
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-//enquanto numeroSecreto for diferente de chute continua
-while(numeroSecreto != chute){
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto){
-        break;
-    } else{
-        if (numeroSecreto > chute){
-            alert(`O número secreto é maior que ${chute}`);
+function exibirTextoNaTela(tag,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela('h1','Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} `;
+        exibirTextoNaTela('p',mensagemTentativas);
+    }else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela('p',`O número secreto é menor!`);
         }else {
-            alert(`O número secreto é menor que ${chute}`);
+            exibirTextoNaTela('p',`O número secreto é maior!`);
         }
-        tentativas ++;
+        tentativas++;
     }
 }
-//Operador ternario.
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
 
+function gerarNumeroAleatorio(numeroMaximo) {
+    return parseInt(Math.random() * numeroMaximo +1);
+}
 
-
+exibirTextoNaTela('h1','Mind Reader');
+exibirTextoNaTela('p','Escolha um numero entre 1 e 10:');
 
 
